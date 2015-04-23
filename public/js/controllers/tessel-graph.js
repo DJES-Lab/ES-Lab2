d6284549524586cccbbae1f5c11dde50711669f2
Derek Nien <aegis60918@gmail.com>
2015-04-23 11:28:56 +0800

    - Add: new post route support for posting array of data objects; Update: client testing post array data

--- public/js/controllers/tessel-graph.js
+++ public/js/controllers/tessel-graph.js
@@ -3,147 +3,148 @@
  */
 angular.module('app')
     .controller('tesselGraphController', function ($scope, $http, $location) {
-        //$scope.testData = [
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 3
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.5
-        //        },
-        //        gpsData: {
-        //            lat: 4.124, lng: 5.252
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 3
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.5
-        //        },
-        //        gpsData: {
-        //            lat: 4.124, lng: 5.252
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 3
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.5
-        //        },
-        //        gpsData: {
-        //            lat: 4.124, lng: 5.252
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 3
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.5
-        //        },
-        //        gpsData: {
-        //            lat: 14.124, lng: 5.2521
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 3
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.55
-        //        },
-        //        gpsData: {
-        //            lat: 4.124, lng: 5.252222
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 32
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.5
-        //        },
-        //        gpsData: {
-        //            lat: 4.124, lng: 5.252
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 3
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.5
-        //        },
-        //        gpsData: {
-        //            lat: 11.124, lng: 5.252
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 3
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.5
-        //        },
-        //        gpsData: {
-        //            lat: 4.1224, lng: 5.252
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:2, z: 3
-        //        },
-        //        climateData: {
-        //            degree: 2, humidity: 1.5
-        //        },
-        //        gpsData: {
-        //            lat: 4.124, lng: 5.252
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 1, y:22, z: -3
-        //        },
-        //        climateData: {
-        //            degree: 23, humidity: 1.225
-        //        },
-        //        gpsData: {
-        //            lat: 4.124, lng: 5.2522
-        //        },
-        //        time: new Date().toString()
-        //    },
-        //    {
-        //        accelerometerData: {
-        //            x: 11, y:-2, z: 3.2
-        //        },
-        //        climateData: {
-        //            degree: 23, humidity: 1.15
-        //        },
-        //        gpsData: {
-        //            lat: 1.234, lng: 3.22
-        //        },
-        //        time: new Date().toString()
-        //    }
-        //];
-        //
-        //$scope.testData.forEach(function(data) {
-        //    $http.post('api/tessel/data', data)
-        //        .success(function(data, status) {
-        //            console.log(data);
-        //        });
-        //});
+        $scope.testData = [
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 3
+                },
+                climateData: {
+                    degree: 2, humidity: 1.5
+                },
+                gpsData: {
+                    lat: 4.124, lng: 5.252
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 3
+                },
+                climateData: {
+                    degree: 2, humidity: 1.5
+                },
+                gpsData: {
+                    lat: 4.124, lng: 5.252
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 3
+                },
+                climateData: {
+                    degree: 2, humidity: 1.5
+                },
+                gpsData: {
+                    lat: 4.124, lng: 5.252
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 3
+                },
+                climateData: {
+                    degree: 2, humidity: 1.5
+                },
+                gpsData: {
+                    lat: 14.124, lng: 5.2521
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 3
+                },
+                climateData: {
+                    degree: 2, humidity: 1.55
+                },
+                gpsData: {
+                    lat: 4.124, lng: 5.252222
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 32
+                },
+                climateData: {
+                    degree: 2, humidity: 1.5
+                },
+                gpsData: {
+                    lat: 4.124, lng: 5.252
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 3
+                },
+                climateData: {
+                    degree: 2, humidity: 1.5
+                },
+                gpsData: {
+                    lat: 11.124, lng: 5.252
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 3
+                },
+                climateData: {
+                    degree: 2, humidity: 1.5
+                },
+                gpsData: {
+                    lat: 4.1224, lng: 5.252
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:2, z: 3
+                },
+                climateData: {
+                    degree: 2, humidity: 1.5
+                },
+                gpsData: {
+                    lat: 4.124, lng: 5.252
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 1, y:22, z: -3
+                },
+                climateData: {
+                    degree: 23, humidity: 1.225
+                },
+                gpsData: {
+                    lat: 4.124, lng: 5.2522
+                },
+                time: new Date().toString()
+            },
+            {
+                accelerometerData: {
+                    x: 11, y:-2, z: 3.2
+                },
+                climateData: {
+                    degree: 23, humidity: 1.15
+                },
+                gpsData: {
+                    lat: 1.234, lng: 3.22
+                },
+                time: new Date().toString()
+            }
+        ];
+
+        $scope.postTestData = function() {
+            $http.post('api/tessel/data/arr', $scope.testData)
+                .success(function (data, status) {
+                    console.log(data);
+                    $scope.getTesselData();
+                });
+        };
 
         $scope.refreshCharts = function () {
             for (var i = 0; i < nv.graphs.length; i++) {
@@ -158,16 +159,19 @@ angular.module('app')
         };
 
         $scope.tesselData = [];
-        $scope.accelGraphData = [{
-            key: 'x',
-            values: []
-        }, {
-            key: 'y',
-            values: []
-        }, {
-            key: 'z',
-            values: []
-        }];
+
+        var initGraphData = function() {
+            $scope.accelGraphData = [{
+                key: 'x',
+                values: []
+            }, {
+                key: 'y',
+                values: []
+            }, {
+                key: 'z',
+                values: []
+            }];
+        };
 
         $scope.getTesselData = function() {
             $http({
@@ -175,6 +179,8 @@ angular.module('app')
                 url: 'api/tessel/data'
             })
                 .success(function (data, status, headers, config) {
+                    $scope.tesselData.length = 0;
+                    initGraphData();
                     $scope.tesselData = data;
                     $scope.accelGraphData.map(function(obj, index) {
                         $scope.tesselData.map(function(data, dataIndex) {
@@ -194,7 +200,6 @@ angular.module('app')
             })
                 .success(function (data, status, headers, config) {
                     $location.path('/tessel-graph');
-                    $scope.tesselData = _.without($scope.tesselData, data);
                     $scope.getTesselData();
                 });
         };
